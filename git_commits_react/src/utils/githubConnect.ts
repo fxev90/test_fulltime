75f8ab38d4cb993105502de40a50765761bbae82
Francisco Escalante <fxev90@gmail.com>
2023-10-08 19:52:06 -0400

    feat: Modify useFetchData to Support Pagination
    - Modify useFetchData function to accept 'page' and 'limit' parameters.
    - Update Axios GET request to include pagination query params.
    - Refactor existing code to work with the new pagination logic.

--- git_commits_react/src/utils/githubConnect.ts
+++ git_commits_react/src/utils/githubConnect.ts
@@ -3,16 +3,14 @@ import { FetchParams } from './../types/Commit';
 import { useQuery } from 'react-query';
 
 
-
-export const useFetchData = ({ url, queryKey, queryParams }: FetchParams) => {
+export const useFetchData = ({ url, queryKey, queryParams}: FetchParams) => {
+    const { page, limit } = queryParams || {};
   const fetcher = async () => {
-    const res = await axios.get(url, {
-      params: queryParams,
-    });
+    const res = await axios.get(`${url}?page=${page}&limit=${limit}`);
     return res.data;
   };
 
-  const { data, isLoading, isError } = useQuery(queryKey, fetcher);
+  const { data, isLoading, isError } = useQuery([queryKey, page, limit], fetcher);
 
   return { data, isLoading, isError };
 };

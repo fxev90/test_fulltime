de3fc2024c382693dc882d66a4610074a10a0e68
Francisco Escalante <fxev90@gmail.com>
2023-10-08 12:35:51 -0400

    Add unit tests for GithubController's getCommits method
    - Mock GithubService to isolate controller logic
    - Test if getCommits in GithubController calls GithubService with correct parameters and returns expected results

--- git_commits_nest/src/github/github.controller.spec.ts
+++ git_commits_nest/src/github/github.controller.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GithubController } from './github.controller';
+import { GithubService } from './github.service';
 
 describe('GithubController', () => {
   let controller: GithubController;
+  const mockGithubService = {
+    getCommits: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [GithubController],
+      providers: [
+        {
+          provide: GithubService,
+          useValue: mockGithubService,
+        },
+      ],
     }).compile();
 
     controller = module.get<GithubController>(GithubController);
@@ -14,5 +24,17 @@ describe('GithubController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return commits from the service', async () => {
+    const mockCommits = [{ id: 'commit1' }, { id: 'commit2' }];
+    mockGithubService.getCommits.mockReturnValue(mockCommits);
+
+    const owner = 'fxev90';
+    const repo = 'test_fulltime';
+    const commits = await controller.getCommits(owner, repo);
+
+    expect(commits).toBe(mockCommits);
+    expect(mockGithubService.getCommits).toHaveBeenCalledWith(owner, repo);
   });
 });

f51a99cdd6866630cd2ea84f55d6e322f06cf0f2
Francisco Escalante <fxev90@gmail.com>
2023-10-08 13:06:57 -0400

    Refactor MeilisearchService tests to fix type issues and add new test cases
    - Use Partial<Index> to relax type constraints for mockIndex
    - Mock MeiliSearch class to simulate its behavior
    - Add test cases for getOrCreateIndex, addDocuments, and search methods
    - Correct the SearchResponse mock to align with the actual interface

--- git_commits_nest/src/meilisearch/meilisearch.service.spec.ts
+++ git_commits_nest/src/meilisearch/meilisearch.service.spec.ts
@@ -1,12 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MeilisearchService } from './meilisearch.service';
+import { ConfigService } from '@nestjs/config';
+import MeiliSearch, { Index, SearchResponse } from 'meilisearch';
+
+jest.mock('meilisearch');
 
 describe('MeilisearchService', () => {
   let service: MeilisearchService;
+  let mockIndex: Partial<Index>;
 
   beforeEach(async () => {
+    mockIndex = {
+      addDocuments: jest.fn(),
+      search: jest.fn(),
+    };
+
+    (MeiliSearch as jest.MockedClass<typeof MeiliSearch>).mockImplementation(
+      () => {
+        return {
+          getIndex: jest.fn().mockResolvedValue(mockIndex),
+          createIndex: jest.fn().mockResolvedValue(mockIndex),
+          index: jest.fn().mockReturnValue(mockIndex),
+        } as unknown as MeiliSearch;
+      },
+    );
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MeilisearchService],
+      providers: [
+        MeilisearchService,
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn().mockReturnValue('some_value'),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<MeilisearchService>(MeilisearchService);
@@ -14,5 +42,38 @@ describe('MeilisearchService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getOrCreateIndex', () => {
+    it('should return existing index if it exists', async () => {
+      const result = await service.getOrCreateIndex('some-index');
+      expect(result).toEqual(mockIndex);
+    });
+  });
+
+  describe('addDocuments', () => {
+    it('should add documents to the index', async () => {
+      const documents = [{ id: 1, name: 'doc1' }];
+      await service.addDocuments('some-index', documents);
+      expect(mockIndex.addDocuments).toHaveBeenCalledWith(documents);
+    });
+  });
+
+  describe('search', () => {
+    it('should perform a search query', async () => {
+      const mockSearchResponse: SearchResponse<any> = {
+        hits: [],
+        offset: 0,
+        limit: 10,
+        exhaustiveNbHits: true,
+        processingTimeMs: 10,
+        query: 'query',
+      };
+
+      mockIndex.search.mockResolvedValue(mockSearchResponse);
+
+      const result = await service.search('some-index', 'query', '10', '1');
+      expect(result).toEqual(mockSearchResponse);
+    });
   });
 });

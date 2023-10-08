669a971649aa17d1f420bba189d557da68d9fa6b
Francisco Escalante <fxev90@gmail.com>
2023-10-08 13:36:19 -0400

    Add default values to Meilisearchconrtroller endpoint search query parameters
    - Set default index name to 'dcommits'
    - Initialize query string to an empty string
    - Set default limit to '10' and default page to '1'

--- git_commits_nest/src/meilisearch/meilisearch.controller.ts
+++ git_commits_nest/src/meilisearch/meilisearch.controller.ts
@@ -4,16 +4,15 @@ import { SearchResponse } from 'meilisearch';
 
 @Controller('search')
 export class MeilisearchController {
-  constructor(private readonly meilisearchService: MeilisearchService) { }
+  constructor(private readonly meilisearchService: MeilisearchService) {}
 
   @Get()
   async search(
-    @Query('index') indexName: string,
-    @Query('query') query: string,
-    @Query('limit') limit: string,
-    @Query('page') page: string,
+    @Query('index') indexName: string = 'commits',
+    @Query('query') query: string = '',
+    @Query('limit') limit: string = '10',
+    @Query('page') page: string = '1',
   ): Promise<SearchResponse<any>> {
-
     return await this.meilisearchService.search(indexName, query, limit, page);
   }
 }

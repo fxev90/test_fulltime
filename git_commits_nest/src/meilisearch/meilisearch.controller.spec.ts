21605d0b51ef6f8fddad6d9df7777d50187b7fff
Francisco Escalante <fxev90@gmail.com>
2023-10-08 13:16:05 -0400

    Update unit tests for MeilisearchController
    - Mock MeilisearchService to isolate controller behavior
    - Add test case for search method to verify service interaction and response

--- git_commits_nest/src/meilisearch/meilisearch.controller.spec.ts
+++ git_commits_nest/src/meilisearch/meilisearch.controller.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MeilisearchController } from './meilisearch.controller';
+import { MeilisearchService } from './meilisearch.service';
+import { SearchResponse } from 'meilisearch';
 
 describe('MeilisearchController', () => {
   let controller: MeilisearchController;
+  let mockService: jest.Mocked<MeilisearchService>;
 
   beforeEach(async () => {
+    mockService = {
+      search: jest.fn(),
+    } as unknown as jest.Mocked<MeilisearchService>;
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MeilisearchController],
+      providers: [
+        {
+          provide: MeilisearchService,
+          useValue: mockService,
+        },
+      ],
     }).compile();
 
     controller = module.get<MeilisearchController>(MeilisearchController);
@@ -14,5 +27,24 @@ describe('MeilisearchController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('search', () => {
+    it('should call MeilisearchService.search with correct parameters', async () => {
+      const mockSearchResponse: SearchResponse<any> = {
+        hits: [],
+        offset: 0,
+        limit: 10,
+        processingTimeMs: 10,
+        query: 'query',
+      } as any;  // Type assertion here
+
+      mockService.search.mockResolvedValue(mockSearchResponse);
+
+      const result = await controller.search('some-index', 'query', '10', '1');
+
+      expect(mockService.search).toHaveBeenCalledWith('some-index', 'query', '10', '1');
+      expect(result).toEqual(mockSearchResponse);
+    });
   });
 });

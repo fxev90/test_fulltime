9bce549c3d05f0482ef6cadba0cddd805d90c892
Francisco Escalante <fxev90@gmail.com>
2023-10-08 23:02:37 -0400

    set animation to my pagination

--- git_commits_react/src/components/CommitsPage.tsx
+++ git_commits_react/src/components/CommitsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CommitCard from './CommitCard';
 import { useFetchData } from '../utils/githubConnect';
 import { Commit } from '../types/Commit';
@@ -8,18 +8,21 @@ const CommitsPage = () => {
     const [currentPage, setCurrentPage] = useState(1);
     const limit = 5; // Number of items per page
     const [isOpen, setIsOpen] = useState(false);
-    const [selectedCommit, setSelectedCommit] =  useState<Commit | null>(null);
-   
+    const [selectedCommit, setSelectedCommit] = useState<Commit | null>(null);
+    const [animationClass, setAnimationClass] = useState('opacity-0');
+
+    useEffect(() => {
+        setAnimationClass('opacity-100');
+    }, [currentPage]);
+
     const handleCardClick = (commit: Commit) => {
         setSelectedCommit(commit);
         setIsOpen(true);
-        console.log(isOpen)
     };
 
     const handleClose = () => {
         setIsOpen(false);
     };
-
 
     const { data, isLoading, isError } = useFetchData({
         url: 'http://localhost:3000/search',
@@ -33,20 +36,30 @@ const CommitsPage = () => {
     });
 
     const handleNext = () => {
-        setCurrentPage((prevPage) => prevPage + 1);
+        setAnimationClass('opacity-0');
+        setTimeout(() => {
+            setCurrentPage((prevPage) => prevPage + 1);
+        }, 200);
     };
 
     const handlePrevious = () => {
         if (currentPage > 1) {
-            setCurrentPage((prevPage) => prevPage - 1);
+            setAnimationClass('opacity-0');
+            setTimeout(() => {
+                setCurrentPage((prevPage) => prevPage - 1);
+            }, 200);
         }
     };
 
     if (isLoading) return <div>Loading...</div>;
     if (isError) return <div>Error loading commits</div>;
 
+
     return (
-            <div className="p-4 space-y-4 transition-opacity ease-in-out duration-300 ">
+        <div className={`p-4 space-y-4 transition-opacity ease-in-out duration-300 ${animationClass}`}>
+            <div className=" text-2xl font-semibold text-gray-700 mb-4  rounded">
+                GitHub Commits - Page {currentPage}
+            </div>
             <div className="flex flex-col items-left space-y-4">
                 {data.hits.map((hit: Commit) => (
 

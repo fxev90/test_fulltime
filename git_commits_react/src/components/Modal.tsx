4d9a0e792e62f600e72c164eeadb8dbf158458c8
Francisco Escalante <fxev90@gmail.com>
2023-10-08 23:02:48 -0400

    set animation to my modal component

--- git_commits_react/src/components/Modal.tsx
+++ git_commits_react/src/components/Modal.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Commit } from '../types/Commit';
-
 
 type CommitModalProps = {
   isOpen: boolean;
@@ -9,37 +8,35 @@ type CommitModalProps = {
 };
 
 const CommitModal: React.FC<CommitModalProps> = ({ isOpen, onClose, commit }) => {
+  const [animationClass, setAnimationClass] = useState('opacity-0');
+
+  useEffect(() => {
+    if (isOpen) {
+      setAnimationClass('opacity-100');
+    } else {
+      setAnimationClass('opacity-0');
+    }
+  }, [isOpen]);
+
   if (!isOpen) {
     return null;
   }
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50 margin-adjust">
+    <div className="fixed inset-0 flex items-center justify-center z-50">
       {/* Overlay */}
-      <div
-        className={`absolute  inset-0 bg-black transition-opacity ease-out duration-300 ${
-          isOpen ? "opacity-50" : "opacity-0"
-        }`}
-        onClick={onClose}
-      ></div>
-
+      <div className="absolute inset-0 bg-black opacity-50" onClick={onClose}></div>
+      
       {/* Modal Content */}
-      <div
-        className={`bg-white p-6 rounded shadow-lg w-1/2 relative z-10 transform transition-all ease-out duration-300 ${
-          isOpen ? "opacity-100 scale-100" : "opacity-0 scale-90"
-        }`}
-      >
-        <h2 className="text-2xl mb-4">Modal Title</h2>
-       
-        <h2>Commit Details</h2>
+      <div className={`bg-white p-6 rounded-lg shadow-md w-1/2 relative z-10 transform transition-all ease-out duration-300 ${animationClass}`}>
+        <h2 className="text-2xl mb-4">GitHub Commit Details</h2>
+        
         <p><strong>SHA:</strong> {commit.sha}</p>
         <p><strong>Message:</strong> {commit.message}</p>
         <p><strong>Author Name:</strong> {commit.authorName}</p>
         <p><strong>Author Email:</strong> {commit.authorEmail}</p>
-        <button
-          onClick={onClose}
-          className="mt-4 bg-red-500 text-white p-2 rounded"
-        >
+
+        <button onClick={onClose} className="mt-4 bg-red-500 text-white p-2 rounded">
           Close
         </button>
       </div>

12460e8462a7961c565660816ff191eccd8de566
Francisco Escalante <fxev90@gmail.com>
2023-10-08 21:43:15 -0400

    Refactor styles on my commitCard and CommitPage component

--- git_commits_react/src/components/CommitCard.tsx
+++ git_commits_react/src/components/CommitCard.tsx
@@ -7,16 +7,16 @@ const CommitCard: React.FC<Commit> = ({ sha, message, authorName, authorEmail, i
 
     return (
         <div 
-            className={`p-4 w-4/5 cursor-pointer rounded-lg shadow-md flex flex-col mb-4 transition-all duration-300 
-                        ${isHovered ? 'bg-gray-100' : 'bg-gradient-to-b from-transparent to-gray-100'}`}
+            className={`  cursor-pointer rounded-lg pb-5 shadow-md flex flex-col mb-4 transition-all duration-300 
+                        ${isHovered ? 'bg-gray-200' : 'bg-gradient-to-b from-transparent to-gray-100'}`}
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
         >
-            <div className="flex items-center space-x-4 mb-2">
+            <div className="flex items-center space-x-4 m-4">
                 <div className="flex-shrink-0 text-gray-600">{sha.substring(0, 7)}</div>
                 <div className="text-gray-700 flex-grow">{message}</div>
             </div>
-            <div className="text-sm text-gray-500">
+            <div className="text-sm text-gray-500 m-auto mt-2">
                 <div>{authorName}</div>
                 <div>{authorEmail}</div>
                
--- git_commits_react/src/components/CommitsPage.tsx
+++ git_commits_react/src/components/CommitsPage.tsx
@@ -13,6 +13,7 @@ const CommitsPage = () => {
     const handleCardClick = (commit: Commit) => {
         setSelectedCommit(commit);
         setIsOpen(true);
+        console.log(isOpen)
     };
 
     const handleClose = () => {
@@ -45,10 +46,11 @@ const CommitsPage = () => {
     if (isError) return <div>Error loading commits</div>;
 
     return (
-        <div className="p-4 space-y-4">
-            <div className="flex flex-col items-center space-y-4">
+            <div className="p-4 space-y-4 transition-opacity ease-in-out duration-300 ">
+            <div className="flex flex-col items-left space-y-4">
                 {data.hits.map((hit: Commit) => (
-                   <div onClick={() => handleCardClick(hit)} key={hit.id}>
+
+                   <div className=' w-4/5' onClick={() => handleCardClick(hit)} key={hit.id}>
                    <CommitCard 
                      sha={hit.sha} 
                      message={hit.message} 
@@ -59,16 +61,18 @@ const CommitsPage = () => {
                  </div>
                 ))}
                  {isOpen && selectedCommit && <CommitModal isOpen onClose={handleClose} commit={selectedCommit} />}
-                <div className="d-flex">
-                    <button onClick={handlePrevious} className="m-4 px-4 py-2 bg-blue-500 text-white rounded">
+              <div className="flex">
+                    <button onClick={handlePrevious} className="transition-all duration-300 ease-in-out hover:bg-red-600 hover:-translate-y-1 m-4 px-4 py-2 bg-red-500 text-white rounded">
                         Previous
                     </button>
-                    <button onClick={handleNext} className="m-4 px-4 py-2 bg-blue-500 text-white rounded">
+                    <button onClick={handleNext} className="transition-all duration-300 ease-in-out hover:bg-blue-600 hover:-translate-y-1 m-4 px-4 py-2 bg-blue-500 text-white rounded">
                         Next
                     </button>
                 </div>
-            </div>
-        </div>
+                
+            </div> 
+             
+        </div> 
     );
 };
 

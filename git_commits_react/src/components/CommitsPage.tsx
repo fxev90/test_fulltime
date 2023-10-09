8b3aeddad72e2b5a17e0bcb345416e202b273bad
Francisco Escalante <fxev90@gmail.com>
2023-10-08 20:34:03 -0400

    refactor the commit page component to work with the new component modal  - setting the boolean state to manage the open and close for the modal  - passing data with the function handleClickCard for the selected card to the modal with a useState that capture the information of the clicked card  -render the modal component with a condition that observe if the modal is open and the selected Commit state exist

--- git_commits_react/src/components/CommitsPage.tsx
+++ git_commits_react/src/components/CommitsPage.tsx
@@ -2,61 +2,74 @@ import React, { useState } from 'react';
 import CommitCard from './CommitCard';
 import { useFetchData } from '../utils/githubConnect';
 import { Commit } from '../types/Commit';
+import CommitModal from './Modal';
 
 const CommitsPage = () => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const limit = 5; // Number of items per page
+    const [currentPage, setCurrentPage] = useState(1);
+    const limit = 5; // Number of items per page
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedCommit, setSelectedCommit] =  useState<Commit | null>(null);
+   
+    const handleCardClick = (commit: Commit) => {
+        setSelectedCommit(commit);
+        setIsOpen(true);
+    };
 
-  const { data, isLoading, isError } = useFetchData({
-    url: 'http://localhost:3000/search',
-    queryKey: 'commits',
-    queryParams: {
-      index: 'commits',
-      query: '',
-      page: currentPage,
-      limit: limit,
-    },
-  });
- 
+    const handleClose = () => {
+        setIsOpen(false);
+    };
 
-  const handleNext = () => {
-    setCurrentPage((prevPage) => prevPage + 1);
-  };
 
-  const handlePrevious = () => {
-    if (currentPage > 1) {
-      setCurrentPage((prevPage) => prevPage - 1);
-    }
-  };
- 
+    const { data, isLoading, isError } = useFetchData({
+        url: 'http://localhost:3000/search',
+        queryKey: 'commits',
+        queryParams: {
+            index: 'commits',
+            query: '',
+            page: currentPage,
+            limit: limit,
+        },
+    });
 
-  if (isLoading) return <div>Loading...</div>;
-  if (isError) return <div>Error loading commits</div>;
+    const handleNext = () => {
+        setCurrentPage((prevPage) => prevPage + 1);
+    };
 
-  return (
-    <div className="p-4 space-y-4">
-      <div className="flex flex-col items-center space-y-4">
-      {data.hits.map((hit: Commit) => (
-          <CommitCard 
-            key={hit.id} 
-            sha={hit.sha} 
-            message={hit.message} 
-            authorName={hit.authorName} 
-            authorEmail={hit.authorEmail}
-            id={hit.id}
-          />
-        ))}
-        <div className="d-flex">
-          <button onClick={handlePrevious} className="m-4 px-4 py-2 bg-blue-500 text-white rounded">
-            Previous
-          </button>
-          <button onClick={handleNext} className="m-4 px-4 py-2 bg-blue-500 text-white rounded">
-            Next
-          </button>
+    const handlePrevious = () => {
+        if (currentPage > 1) {
+            setCurrentPage((prevPage) => prevPage - 1);
+        }
+    };
+
+    if (isLoading) return <div>Loading...</div>;
+    if (isError) return <div>Error loading commits</div>;
+
+    return (
+        <div className="p-4 space-y-4">
+            <div className="flex flex-col items-center space-y-4">
+                {data.hits.map((hit: Commit) => (
+                   <div onClick={() => handleCardClick(hit)} key={hit.id}>
+                   <CommitCard 
+                     sha={hit.sha} 
+                     message={hit.message} 
+                     authorName={hit.authorName} 
+                     authorEmail={hit.authorEmail}
+                     id={hit.id}
+                   />
+                 </div>
+                ))}
+                 {isOpen && selectedCommit && <CommitModal isOpen onClose={handleClose} commit={selectedCommit} />}
+                <div className="d-flex">
+                    <button onClick={handlePrevious} className="m-4 px-4 py-2 bg-blue-500 text-white rounded">
+                        Previous
+                    </button>
+                    <button onClick={handleNext} className="m-4 px-4 py-2 bg-blue-500 text-white rounded">
+                        Next
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default CommitsPage;
--- git_commits_react/src/components/Modal.tsx
+++ git_commits_react/src/components/Modal.tsx
@@ -1,14 +1,50 @@
 import React from 'react';
+import { Commit } from '../types/Commit';
 
-const CommitModal = () => {
+
+type CommitModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  commit: Commit;
+};
+
+const CommitModal: React.FC<CommitModalProps> = ({ isOpen, onClose, commit }) => {
+  if (!isOpen) {
+    return null;
+  }
+
   return (
-    <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(0,0,0,0.5)' }}>
-      <div style={{ position: 'relative', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', backgroundColor: 'white', padding: '20px' }}>
-        <h3>Commit Details</h3>
-  
-        <button >Close</button>
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      {/* Overlay */}
+      <div
+        className={`absolute inset-0 bg-black transition-opacity ease-out duration-300 ${
+          isOpen ? "opacity-50" : "opacity-0"
+        }`}
+        onClick={onClose}
+      ></div>
+
+      {/* Modal Content */}
+      <div
+        className={`bg-white p-6 rounded shadow-lg w-1/2 relative z-10 transform transition-all ease-out duration-300 ${
+          isOpen ? "opacity-100 scale-100" : "opacity-0 scale-90"
+        }`}
+      >
+        <h2 className="text-2xl mb-4">Modal Title</h2>
+       
+        <h2>Commit Details</h2>
+        <p><strong>SHA:</strong> {commit.sha}</p>
+        <p><strong>Message:</strong> {commit.message}</p>
+        <p><strong>Author Name:</strong> {commit.authorName}</p>
+        <p><strong>Author Email:</strong> {commit.authorEmail}</p>
+        <button
+          onClick={onClose}
+          className="mt-4 bg-red-500 text-white p-2 rounded"
+        >
+          Close
+        </button>
       </div>
     </div>
   );
 };
+
 export default CommitModal;
